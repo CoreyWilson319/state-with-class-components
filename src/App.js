bca1078ec2b38ca0946c2f644b1b6cc3a598d6ff
Your Name <you@example.com>
2020-12-09 11:40:42 -0500

    finished codealong

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+// What does the 2 part import do?
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      count: 0, 
+      inputtedNumber: 0
+    }
+
+    // This works if we don't use the arrow notation on line 18
+    // For the increment function
+    // arrow notation is simpler but both work
+    // this.increment = this.increment.bind(this)
+  }
+
+  increment = () => {
+    // const amountToAdd = prompt('How much do you want to add?')
+
+    const newCount = this.state.count + this.state.inputtedNumber
+
+    this.setState({count: newCount})
+
+
+
+    // let currentCount = this.state.count
+    // let newCount = currentCount + 1
+
+    // this.setState({ count: newCount })
+  }
+
+  decrement = () => {
+    // const amountToDecr = prompt('How much do you want to subtract?')
+
+    const newCount = this.state.count - this.state.inputtedNumber
+
+    this.setState({count: newCount})
+    // this.setState({ count: this.state.count - 1 })
+  }
+
+  reset = () => {
+    this.setState({count: 0})
+  }
+
+  changeInput = (evt) => {
+    // console.log(evt.target.value)
+    const newValue = parseInt(evt.target.value) || ""
+
+    this.setState({inputtedNumber: newValue})
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>{this.state.count}</h1>
+        <input value={this.state.inputtedNumber} onChange={this.changeInput}/>
+        <br></br>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.reset}>reset</button>
+        <button onClick={this.decrement}>-</button>
+      </div>
+    );
+  }
 }
 
 export default App;
